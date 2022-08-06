195d6800166815a292e5b23f9b4a41e1d7ae67b3
splitube <voicesplit@daum.net>
2022-08-06 17:12:10 +0900

    first commit

---
+++ learning/src/test/java/voicesplit/learning/LearningApplicationTests.java
@@ -0,0 +1,13 @@
+package voicesplit.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
